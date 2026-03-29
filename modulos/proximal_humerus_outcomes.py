a5795cef4549864965fa32f34dd72b2fa31b2125
ViniciusBacelarFerreira <viniciusbacelarferreira@gmail.com>
2026-03-29 11:08:35 -0400

    Update proximal_humerus_outcomes.py

--- modulos/proximal_humerus_outcomes.py
+++ modulos/proximal_humerus_outcomes.py
@@ -3,11 +3,9 @@ from utils import gerar_grafico_velocimetro, gerar_grafico_waterfall, obter_text
 from database import salvar_registro
 
 def calcular_adequacao(ases, vas, sane, eq5d, rom, cms, dash):
-    # Cálculo de Adequação do Protocolo de Seguimento (Richard et al., ASES Taskforce 2020)
     pontos = 0.0
     contribs = {}
     
-    # Recomendações Principais (Padrão-Ouro ASES)
     if ases:
         pontos += 25.0
         contribs["Escore ASES"] = 25.0
@@ -28,18 +26,16 @@ def calcular_adequacao(ases, vas, sane, eq5d, rom, cms, dash):
         pontos += 20.0
         contribs["Amplitude de Movimento (ROM)"] = 20.0
 
-    # Ferramentas não recomendadas como primárias pela força-tarefa, mas amplamente usadas
     if cms and not ases:
         contribs["Aviso: Constant-Murley"] = 0.0
     if dash and not sane:
         contribs["Aviso: DASH (Longo)"] = 0.0
 
     adequacao = min(100.0, pontos)
-    
     return adequacao, contribs
 
 def renderizar_ui():
-    st.markdown("<div class='calc-info'><b>O que avalia:</b> Audita a qualidade da recolha de dados pós-operatórios em <b>fraturas do úmero proximal</b>. Baseado nas diretrizes da força-tarefa da <i>American Shoulder and Elbow Surgeons (ASES)</i> para padronização de desfechos clínicos.</div>", unsafe_allow_html=True)
+    st.markdown("<div class='calc-info'><b>O que avalia:</b> Audita a qualidade da coleta de dados pós-operatórios em <b>fraturas do úmero proximal</b>. Baseado nas diretrizes da força-tarefa da <i>American Shoulder and Elbow Surgeons (ASES)</i> para padronização de desfechos clínicos.</div>", unsafe_allow_html=True)
     st.markdown("<div class='input-card'><h4>💪 Qualidade do Seguimento Funcional (Úmero Proximal)</h4>", unsafe_allow_html=True)
     
     st.markdown("<p style='font-size: 0.9rem; color: gray;'>Selecione os instrumentos de avaliação funcional que foram aplicados a este paciente na consulta atual:</p>", unsafe_allow_html=True)
@@ -62,7 +58,7 @@ def renderizar_ui():
         res, contribs = calcular_adequacao(ph_ases, ph_vas, ph_sane, ph_eq5d, ph_rom, ph_cms, ph_dash)
         st.session_state.proximal_humerus_outcomes_res = (res, contribs)
         params_str = f"ASES: {ph_ases} | VAS: {ph_vas} | SANE: {ph_sane} | EQ5D: {ph_eq5d} | ROM: {ph_rom}"
-        salvar_registro("Auditoria Seguimento Úmero P.", res, "melhora", params_str) # Usamos "melhora" para exibir verde em valores altos
+        salvar_registro("Auditoria Seguimento Úmero P.", res, "melhora", params_str) 
         
     if st.session_state.get('proximal_humerus_outcomes_res'):
         res, contribs = st.session_state.proximal_humerus_outcomes_res
