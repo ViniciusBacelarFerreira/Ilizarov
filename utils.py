f540ffb0503c710de27d50280f742aa04b95d43b
ViniciusBacelarFerreira <viniciusbacelarferreira@gmail.com>
2026-03-29 09:21:28 -0400

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+# utils.py
+import plotly.graph_objects as go
+
+def gerar_grafico_waterfall(contribuicoes, titulo="Impacto das Variáveis"):
+    # logica do plotly...
+
+def gerar_grafico_velocimetro(prob, tipo="risco"):
+    # logica do plotly...
